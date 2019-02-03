a0e48a4b6cf4a51fa7603f0bdbb9d49501aeb252
ahmetkotan <ahmtkotan@gmail.com>
2019-02-03 15:35:37 +0300

    cleaned small bugs

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-pip install --upgrade pip
-pip install -r requirements.txt
+if [ -z $VIRTUAL_ENV ];
+    then
+        sudo pip install --upgrade pip
+        sudo pip install -r requirements.txt
+    else
+        pip install --upgrade pip
+        pip install -r requirements.txt
+fi;
 python manage.py migrate
 echo "yes" | python manage.py collectstatic
 python manage.py createsuperuser
